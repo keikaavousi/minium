1dfd9ff0d84017c551ad1d7442fefcd7eda7bc55
saeedkefayati <s.kefayati2012@gmail.com>
2020-04-16 06:45:20 +0430

    landing

--- js/master.js
+++ js/master.js
@@ -1,12 +1,12 @@
 let prebtn = document.getElementsByClassName("btncopy");
 let precode = document.getElementsByClassName("precode");
 for (const i in prebtn) {
-    prebtn[i].addEventListener("click",function(){
-        const el = document.createElement('textarea');
-        el.value = precode[i].textContent;
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    });
+  prebtn[i].addEventListener("click", function () {
+    const el = document.createElement("textarea");
+    el.value = precode[i].textContent;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  });
 }
