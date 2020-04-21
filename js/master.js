9dcf34554923f5f47e4475b6c540cabe440cf31a
saeedkefayati <s.kefayati2012@gmail.com>
2020-04-22 00:04:50 +0430

    copyerror

--- js/master.js
+++ js/master.js
@@ -1,12 +1,22 @@
-let prebtn = document.getElementsByClassName("btncopy");
-let precode = document.getElementsByClassName("precode");
-for (const i in prebtn) {
-  prebtn[i].addEventListener("click", function () {
-    const el = document.createElement("textarea");
-    el.value = precode[i].textContent;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
-  });
+let prebtn = document.getElementsByClassName('btncopy');
+let precode = document.getElementsByClassName('precode');
+// for (const i in prebtn) {
+// 	prebtn[i].addEventListener('click', function () {
+// 		const el = document.createElement('textarea');
+// 		el.value = precode[i].textContent;
+// 		document.body.appendChild(el);
+// 		el.select();
+// 		document.execCommand('copy');
+// 		document.body.removeChild(el);
+// 	});
+// }
+for (let i = 0; i < prebtn.length; i++) {
+	prebtn[i].addEventListener('click', function () {
+		const el = document.createElement('textarea');
+		el.value = precode[i].textContent;
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+	});
 }
