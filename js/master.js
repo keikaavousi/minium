fdf6dc6abe533a705a442ed82f3b4e00a4a75260
MohammadReza Keikavousi <keikaavousi@gmail.com>
2020-04-27 16:57:53 +0430

    optimize styles

--- js/master.js
+++ js/master.js
@@ -1,6 +1,20 @@
 let prebtn = document.getElementsByClassName('btncopy');
 let precode = document.getElementsByClassName('precode');
-// for (const i in prebtn) {
+
+for (const btn of prebtn) {
+	btn.addEventListener('click', function () {
+		const el = document.createElement('textarea');
+    el.value = btn.textContent;
+    console.log(el.value,el.innerHTML);
+		// document.body.appendChild(el);
+		// el.select();
+		// document.execCommand('copy');
+		// document.body.removeChild(el);
+	});
+}
+
+
+// for (let i = 0; i < prebtn.length; i++) {
 // 	prebtn[i].addEventListener('click', function () {
 // 		const el = document.createElement('textarea');
 // 		el.value = precode[i].textContent;
@@ -10,13 +24,3 @@ let precode = document.getElementsByClassName('precode');
 // 		document.body.removeChild(el);
 // 	});
 // }
-for (let i = 0; i < prebtn.length; i++) {
-	prebtn[i].addEventListener('click', function () {
-		const el = document.createElement('textarea');
-		el.value = precode[i].textContent;
-		document.body.appendChild(el);
-		el.select();
-		document.execCommand('copy');
-		document.body.removeChild(el);
-	});
-}
