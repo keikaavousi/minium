4420c5117262fa6a52022142049c53c6c79b6cbb
saeedkefayati <s.kefayati2012@gmail.com>
2020-04-28 00:28:47 +0430

    final

--- js/master.js
+++ js/master.js
@@ -1,26 +1,25 @@
 let prebtn = document.getElementsByClassName('btncopy');
 let precode = document.getElementsByClassName('precode');
 
-for (const btn of prebtn) {
-	btn.addEventListener('click', function () {
-		const el = document.createElement('textarea');
-    el.value = btn.textContent;
-    console.log(el.value,el.innerHTML);
-		// document.body.appendChild(el);
-		// el.select();
-		// document.execCommand('copy');
-		// document.body.removeChild(el);
-	});
-}
-
-
-// for (let i = 0; i < prebtn.length; i++) {
-// 	prebtn[i].addEventListener('click', function () {
+// for (const btn of prebtn) {
+// 	btn.addEventListener('click', function () {
 // 		const el = document.createElement('textarea');
-// 		el.value = precode[i].textContent;
-// 		document.body.appendChild(el);
-// 		el.select();
-// 		document.execCommand('copy');
-// 		document.body.removeChild(el);
+//     el.value = btn.textContent;
+//     console.log(el.value,el.innerHTML);
+// 		// document.body.appendChild(el);
+// 		// el.select();
+// 		// document.execCommand('copy');
+// 		// document.body.removeChild(el);
 // 	});
 // }
+
+for (let i = 0; i < prebtn.length; i++) {
+	prebtn[i].addEventListener('click', function () {
+		const el = document.createElement('textarea');
+		el.value = precode[i].textContent;
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+	});
+}
