c47dfbf5d0a585052e862dc77e5c2a423e08f37e
saeedkefayati <s.kefayati2012@gmail.com>
2020-04-30 18:10:50 +0430

    end

--- js/master.js
+++ js/master.js
@@ -13,8 +13,18 @@ let precode = document.getElementsByClassName('precode');
 // 	});
 // }
 
+// for (let i = 0; i < prebtn.length; i++) {
+// 	prebtn[i].addEventListener('click', function () {
+// 		const el = document.createElement('textarea');
+// 		el.value = precode[i].textContent;
+// 		document.body.appendChild(el);
+// 		el.select();
+// 		document.execCommand('copy');
+// 		document.body.removeChild(el);
+// 	});
+// }
 for (let i = 0; i < prebtn.length; i++) {
-	prebtn[i].addEventListener('click', function () {
+	prebtn[i].addEventListener('click', () => {
 		const el = document.createElement('textarea');
 		el.value = precode[i].textContent;
 		document.body.appendChild(el);
