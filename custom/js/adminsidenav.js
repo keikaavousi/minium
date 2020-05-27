3a88b8669bbcdb4c798db9f65af72e566e45edc8
saeedkefayati <s.kefayati2012@gmail.com>
2020-05-27 12:51:56 +0430

    check

--- custom/js/adminsidenav.js
+++ custom/js/adminsidenav.js
@@ -1,21 +1,21 @@
-let btnmenu = document.querySelector("#menubtn");
-let sidebar = document.getElementsByTagName("ul")[0];
-let content = document.getElementsByClassName("content-wrapper")[0];
+let btnmenu = document.querySelector('#menubtn');
+let sidebar = document.getElementsByTagName('ul')[0];
+let content = document.getElementsByClassName('content-wrapper')[0];
 let menutoggle = false;
 
-btnmenu.addEventListener("click", function () {
-  menutoggle = !menutoggle;
-  if (menutoggle) {
-    // btnmenu.children[0].innerHTML = `
-    // <line x1="18" y1="6" x2="6" y2="18"></line>
-    // <line x1="6" y1="6" x2="18" y2="18"></line>`;
-    sidebar.classList.add("sidetoggle");
-    content.classList.add("contenttoggle");
-  } else {
-    // btnmenu.children[0].innerHTML = `<line x1="3" y1="12" x2="21" y2="12"></line>
-    // <line x1="3" y1="6" x2="21" y2="6"></line>
-    // <line x1="3" y1="18" x2="21" y2="18"></line>`;
-    sidebar.classList.remove("sidetoggle");
-    content.classList.remove("contenttoggle");
-  }
+btnmenu.addEventListener('click', function () {
+	menutoggle = !menutoggle;
+	if (menutoggle) {
+		sidebar.classList.add('sidetoggle');
+		content.classList.add('contenttoggle');
+		// btnmenu.children[0].innerHTML = `
+		// <line x1="18" y1="6" x2="6" y2="18"></line>
+		// <line x1="6" y1="6" x2="18" y2="18"></line>`;
+	} else {
+		sidebar.classList.remove('sidetoggle');
+		content.classList.remove('contenttoggle');
+		// btnmenu.children[0].innerHTML = `<line x1="3" y1="12" x2="21" y2="12"></line>
+		// <line x1="3" y1="6" x2="21" y2="6"></line>
+		// <line x1="3" y1="18" x2="21" y2="18"></line>`;
+	}
 });
