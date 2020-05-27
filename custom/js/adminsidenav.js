bcdb647f140d95d50d46ea2b22962aa3df5e86ee
saeedkefayati <s.kefayati2012@gmail.com>
2020-05-27 12:55:09 +0430

    check

--- custom/js/adminsidenav.js
+++ custom/js/adminsidenav.js
@@ -8,14 +8,8 @@ btnmenu.addEventListener('click', function () {
 	if (menutoggle) {
 		sidebar.classList.add('sidetoggle');
 		content.classList.add('contenttoggle');
-		// btnmenu.children[0].innerHTML = `
-		// <line x1="18" y1="6" x2="6" y2="18"></line>
-		// <line x1="6" y1="6" x2="18" y2="18"></line>`;
 	} else {
 		sidebar.classList.remove('sidetoggle');
 		content.classList.remove('contenttoggle');
-		// btnmenu.children[0].innerHTML = `<line x1="3" y1="12" x2="21" y2="12"></line>
-		// <line x1="3" y1="6" x2="21" y2="6"></line>
-		// <line x1="3" y1="18" x2="21" y2="18"></line>`;
 	}
 });
